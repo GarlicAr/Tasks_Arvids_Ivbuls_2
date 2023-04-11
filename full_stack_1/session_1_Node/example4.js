a4fd88be7dca049eafcf6a6595f45b7651564da1
Arvids Ivbuls <arvidsivbuls@gmail.com>
2023-04-11 15:48:46 +0300

    ADD: functions

--- full_stack_1/session_1_Node/example4.js
+++ full_stack_1/session_1_Node/example4.js
@@ -17,3 +17,28 @@ console.log(result);
 var result = '1' + 1;
 console.log(result);
 */
+
+
+function add(var1, var2){
+    var result = var1 + var2;
+    return result;
+}
+
+console.log(add(3,2));
+
+
+var elements = [1,2];
+var elements2 = [2,4];
+
+elements = elements2;
+
+function addElement(inp){
+    inp.push(3);
+}
+
+
+
+addElement(elements);
+addElement(elements); 
+
+console.log(elements, elements2); // 2 4 3 3 
