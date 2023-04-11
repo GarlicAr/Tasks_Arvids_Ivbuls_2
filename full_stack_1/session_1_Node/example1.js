1aeb033d12444ff6147f65dc8776bc65ae8d7ebc
Arvids Ivbuls <arvidsivbuls@gmail.com>
2023-04-11 14:17:13 +0300

    Started learning JavaScript

--- full_stack_1/session_1_Node/example1.js
+++ full_stack_1/session_1_Node/example1.js
@@ -1,1 +1,43 @@
 console.log('Hello World!')
+
+
+// basic types
+
+var shopName="Rimi";
+
+// boolean
+
+var isShopOpen=true;
+
+// number
+
+var peopleInShop=200;
+var priceOfCheese = 12.5;
+
+// not strongly typed
+
+isShopOpen = 10;
+isShopOpen = false;
+
+revenueYear = 1e6; // if not var in front then global
+strStatement= 'Year Revenue: ' + revenueYear.toString();
+
+minValue= Number.MIN_VALUE;
+
+console.log(strStatement);
+
+strStatement = strStatement.substring(strStatement.indexOf(':')+1);
+
+console.log(strStatement);
+
+isEUR = strStatement.includes('EUR');
+
+console.log(isEUR);
+
+if(!isEUR){
+    var revenueEUR = parseFloat(strStatement); //parseInt
+    var statementFull = `Year revenue: ${revenueEUR} EUR` ; //Tilde
+    console.log(statementFull);
+}
+
+
