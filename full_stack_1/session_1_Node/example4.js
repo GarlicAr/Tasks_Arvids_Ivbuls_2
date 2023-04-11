9d52c00b27ec72f5a56cc17c83ab136ae4accb4b
Arvids Ivbuls <arvidsivbuls@gmail.com>
2023-04-11 15:38:21 +0300

    ADD: example4

---
+++ full_stack_1/session_1_Node/example4.js
@@ -0,0 +1,19 @@
+var a = `1`;
+var result = 0;
+// ===
+if(a === 1){
+    result+=a;
+}
+if(typeof(a) === `string`){
+    console.log(`string type`)
+}
+
+console.log(result);
+
+/*
+var result = 1 + '1';
+console.log(result);
+
+var result = '1' + 1;
+console.log(result);
+*/
