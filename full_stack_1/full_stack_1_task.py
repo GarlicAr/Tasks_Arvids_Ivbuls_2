4257fed12ceef92608facce16f2ed49a6f46648b
Arvids Ivbuls <arvidsivbuls@gmail.com>
2023-04-11 14:26:04 +0300

    Correction to full_stack_task_1

--- full_stack_1/full_stack_1_task.py
+++ full_stack_1/full_stack_1_task.py
@@ -6,3 +6,11 @@ def f(x, b):
     for i in range(1, b):
         temp *= x * b
     return temp
+
+
+skaitlis = int(input("Ievadi skaitli X:\n"))
+pakape = int(input("Ievadi pakāpi N:\n"))
+
+result = f(skaitlis, pakape)
+
+print(f"{skaitlis}*{pakape}={skaitlis*pakape}^{pakape}= {result}")
