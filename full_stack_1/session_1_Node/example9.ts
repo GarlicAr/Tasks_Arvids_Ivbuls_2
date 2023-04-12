d70545ac7d906cb31c2423e3f8f0db488dd642e2
Arvids Ivbuls <arvidsivbuls@gmail.com>
2023-04-12 20:10:50 +0300

    Example10, TypeScript, simpleCast, simpleCar

--- full_stack_1/session_1_Node/example9.ts
+++ full_stack_1/session_1_Node/example9.ts
@@ -1,7 +1,35 @@
-console.log(`hello typescript`);
+
 
 let cart1: number = 0;
 
 //Shadi nevar typescript => cart1=`2`;
 
-console.log(cart1);
+let shopCarts: number[] = [2,3,4];
+
+shopCarts.push(5);
+
+let simpleTuple: [number, number, string] = [3,4, `item`];
+
+let simpleDict = {
+    "brand": "bmw",
+    "km": 2e5,
+    "isManual": true
+};
+
+interface Car {
+    brand: string,
+    km: number,
+    isManual: boolean
+}
+
+
+let simpleCar: Car = {
+    brand: "BMW",
+    km: 2e5,
+    isManual: true
+};
+
+let simpleCast: Car = simpleDict;
+
+
+console.log(simpleCar, simpleCast);
